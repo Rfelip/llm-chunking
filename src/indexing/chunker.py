9ba211f4cb48947617b5d2e6d8d928b51f950cd6
Ruan Felipe <ruanfelipe@matematica.ufrj.br>
2025-02-11 10:49:15 -0300

    Added retrieval do embedder

--- src/indexing/chunker.py
+++ src/indexing/chunker.py
@@ -1,18 +1,10 @@
-from .indexing_tree import build_index_from_url
-from langchain.text_splitter import NLTKTextSplitter
-import nltk
 import os
+import numpy as np
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-nltk_data_dir = "../../data/ntlk_data"
-os.makedirs(nltk_data_dir, exist_ok=True)
-nltk.download('punkt', download_dir=nltk_data_dir)
-nltk.download('punkt_tab', download_dir=nltk_data_dir)
-nltk.data.path.append(nltk_data_dir)
 
-def build_chunks_from_tree(tree, chunk_size = 512):
+def build_chunks_from_tree(tree, chunk_size = 384):
     text_to_be_chunked = tree.root.text
-    splitter = NLTKTextSplitter(chunk_size=chunk_size,
-                                        chunk_overlap=20)
-    
-    docs = splitter.create_documents([text_to_be_chunked])
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=28)
+    docs = np.array(text_splitter.split_text(text_to_be_chunked))
     return docs
--- src/indexing/embedder.py
+++ src/indexing/embedder.py
@@ -4,27 +4,25 @@ from langchain_huggingface import HuggingFaceEmbeddings
 import faiss
 
 class EmbeddingManager:
-    def __init__(self, model_name='all-MiniLM-L6-v2', model_dir='data/models', index_dir='data/faiss_index'):
+    def __init__(self, model_name="all-mpnet-base-v2", model_dir='data/models', index_dir='data/faiss_index'):
         self.model_name = model_name
         self.model_dir = model_dir
         self.index_dir = index_dir
         self.model = None
         self.index = None
-
+        self.chunks = None
         os.makedirs(self.model_dir, exist_ok=True)
         os.makedirs(self.index_dir, exist_ok=True)
 
     def load_or_download_model(self):
         """Load model from local directory or download if not exists"""
+        model_kwargs = {'device': 'cpu'}
+        encode_kwargs = {'normalize_embeddings': True, 'convert_to_numpy' : True}
         model_path = os.path.join(self.model_dir, self.model_name)
         
-        if os.path.exists(model_path):
-            print("Loading model from local directory...")
-            self.model = HuggingFaceEmbeddings(cache_folder = model_path, model_name = self.model_name)
-        else:
-            print("Downloading model...")
-            self.model = HuggingFaceEmbeddings(cache_folder = model_path, model_name = self.model_name)
-            self.model.save(model_path)
+        print("Loading model from local directory or downloading it...")
+        self.model = HuggingFaceEmbeddings(cache_folder = model_path, model_name = self.model_name,
+                                            encode_kwargs=encode_kwargs, model_kwargs = model_kwargs)
         return self.model
 
     def generate_embeddings(self, chunks):
@@ -33,12 +31,8 @@ class EmbeddingManager:
             self.load_or_download_model()
             
         print("Generating embeddings...")
-        return self.model.embed_documents(chunks)
-
-    def normalize_embeddings(self, embeddings):
-        """Normalize embeddings to unit length"""
-        norms = np.linalg.norm(embeddings, axis=1, keepdims=True)
-        return embeddings / norms
+        embbed = self.model.embed_documents(chunks)
+        return embbed
 
     def create_faiss_index(self, embeddings):
         """Create and save FAISS index"""
@@ -73,17 +67,24 @@ class EmbeddingManager:
         print(f"Loaded index with {self.index.ntotal} vectors")
         return self.index
 
-    def __call__(self, chunks=None, index_name = "index"):
+    def generate_index_from_chunks(self, chunks=None, index_name = "index"):
         """Check for existing index, load if exists; else create from chunks and save."""
         index_path = os.path.join(self.index_dir, f"{index_name}.index")
         if os.path.exists(index_path):
             self.load_faiss_index(index_name)
+            self.chunks = chunks
         else:
             if chunks is None:
                 raise ValueError("Chunks must be provided to create a new index.")
-            
-            text_chunks = [doc.page_content for doc in chunks]
-            embeddings = self.generate_embeddings(text_chunks)
+            self.chunks = chunks
+            embeddings = self.generate_embeddings(chunks)
             self.create_faiss_index(embeddings)
             self.save_faiss_index(index_name)
         return self.index
+    
+    def search_index(self, query, k = 3):
+        query = np.array(query)
+        query_vector = np.array(self.generate_embeddings(query))
+        SIMILARITY, IDs = self.index.search(query_vector, k)
+        resulting_chunks = self.chunks[IDs]
+        return SIMILARITY, resulting_chunks
